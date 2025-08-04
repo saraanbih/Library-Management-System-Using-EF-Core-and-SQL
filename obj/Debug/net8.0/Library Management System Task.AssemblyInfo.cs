46c9e17888479c75ab30d413dda8bf6b102acf37
Sara Nabih <saranabih@gmail.com>
2025-08-04 23:33:14 +0300

    Initial commit

--- obj/Debug/net8.0/Library Management System Task.AssemblyInfo.cs
+++ obj/Debug/net8.0/Library Management System Task.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library Management System Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d56c145a89c7666cebe1c9d8776e0659414c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library Management System Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library Management System Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
